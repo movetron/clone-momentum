1b1bf04e7dec6314c0d2e9629608680846242978
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2024-10-14 00:31:43 +0300

    Update lang-weather.js

--- js/lang-weather.js
+++ js/lang-weather.js
@@ -1,28 +1,8 @@
 const dictionary = {
-  ru: {
-    settings: {
-      weather: 'Погода',
-    },
-
     weather: {
       input: 'Введите город',
       humidity: 'Влажность',
       wind: 'Скорость ветра'
     },
-
-    
-  },
-
-  en: {
-    settings: {
-      weather: 'Weather',
-    },
   
-    weather: {
-      input: 'Enter city',
-      humidity: 'Humidity',
-      wind: 'Wind speed'
-    },
-
-  }
 }
